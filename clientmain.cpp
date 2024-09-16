c9e229f4b650ab1a08cb52f236036506c305bef8
sushma vendarapu <suve19@student.bth.se>
2024-09-16 10:42:02 +0000

    This commit adds the TCP Connection to the client.

--- clientmain.cpp
+++ clientmain.cpp
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <errno.h>
 /* You will to add includes here */
 
 // Enable if you want debugging to be printed, see examble below.
@@ -25,9 +29,39 @@ int main(int argc, char *argv[]){
 
   /* Do magic */
   int port=atoi(Destport);
-#ifdef DEBUG 
-  printf("Host %s, and port %d.\n",Desthost,port);
-#endif
 
+  if (port <= 0) {
+        fprintf(stderr, "Invalid port number.\n");
+        return 1;
+    }
+
+    printf("Host: %s, Port: %d\n", Desthost, port);
+
+    // Create a socket for the TCP connection using SOCK_STREAM.
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        perror("socket");
+        return 1;
+    }
+
+    // Set up the server address structure.
+    struct sockaddr_in servaddr;
+    memset(&servaddr, 0, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(port);  // Convert port to network byte order.
+
+    // Convert the IP address to binary form and store it in servaddr.
+    if (inet_pton(AF_INET, Desthost, &servaddr.sin_addr) <= 0) {
+        perror("inet_pton");
+        close(sockfd);
+        return 1;
+    }
+
+    // Attempt to connect to the server.
+    if (connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
+        perror("connect");
+        close(sockfd);
+        return 1;
+    }
   
 }
